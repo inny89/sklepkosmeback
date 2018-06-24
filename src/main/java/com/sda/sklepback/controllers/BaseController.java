a71ede030255a175c6d18e4affcc7a2962242680
milosz <milosztadrzak@gmail.com>
2018-06-24 11:30:25 +0200

    Entity poprawione

--- src/main/java/com/sda/sklepback/controllers/BaseController.java
+++ src/main/java/com/sda/sklepback/controllers/BaseController.java
@@ -1,4 +1,20 @@
 package com.sda.sklepback.controllers;
 
+
+import com.sda.sklepback.model.Item;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/item")
 public class BaseController {
+
+
+    @RequestMapping(path = "/test", method = RequestMethod.GET)
+    public ResponseEntity<Item> test() {
+        return ResponseEntity.ok(new Item());
+    }
+
 }
--- src/main/java/com/sda/sklepback/model/Item.java
+++ src/main/java/com/sda/sklepback/model/Item.java
@@ -1,4 +1,25 @@
 package com.sda.sklepback.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@Getter
+@Setter
+@NoArgsConstructor
+@Table
 public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "Name")
+    private String name;
+
 }
