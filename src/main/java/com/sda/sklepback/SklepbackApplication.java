22ca017fa7c01c2d8945b3b256dde9ad87bebe7d
milosz <milosztadrzak@gmail.com>
2018-06-24 10:46:28 +0200

    Pierwszy commit

---
+++ src/main/java/com/sda/sklepback/SklepbackApplication.java
@@ -0,0 +1,12 @@
+package com.sda.sklepback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SklepbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SklepbackApplication.class, args);
+	}
+}
